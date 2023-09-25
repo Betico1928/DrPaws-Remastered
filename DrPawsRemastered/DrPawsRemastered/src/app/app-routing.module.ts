460bbfd613bfd9e9e6a5757ac10ea070ce11b685
Betico1928 <betoveluis@gmail.com>
2023-09-24 23:08:28 -0500

    Routings entre el home y el dashboard de veterinarios.

--- DrPawsRemastered/DrPawsRemastered/src/app/app-routing.module.ts
+++ DrPawsRemastered/DrPawsRemastered/src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {LandingPageComponent} from "./landing-page/landing-page.component";
+import {LoginAdministrativoComponent} from "./login-administrativo/login-administrativo.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  // Default route
+  {path: '', pathMatch: 'full', redirectTo: 'home'},
+
+  // Eagerly loaded routes
+  {path: 'home', component: LandingPageComponent},
+  {path: 'login-administrativo', component: LoginAdministrativoComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
