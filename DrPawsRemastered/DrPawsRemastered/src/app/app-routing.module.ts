e3a3b8b78769d3dbc11da2d4eaf68f8807ce6d23
Betico1928 <betoveluis@gmail.com>
2023-09-24 23:48:13 -0500

    Página de 'crear-mascota' portada correctamente 👍

--- DrPawsRemastered/DrPawsRemastered/src/app/app-routing.module.ts
+++ DrPawsRemastered/DrPawsRemastered/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import {LandingPageComponent} from "./landing-page/landing-page.component";
 import {LoginAdministrativoComponent} from "./login-administrativo/login-administrativo.component";
 import {DashboardVeterinarioComponent} from "./dashboard-veterinario/dashboard-veterinario.component";
+import {CrearMascotaComponent} from "./crud-mascotas/crear-mascota/crear-mascota.component";
 
 const routes: Routes = [
   // Default route
@@ -13,6 +14,9 @@ const routes: Routes = [
 
   {path: 'login-administrativo', component: LoginAdministrativoComponent},
   {path: 'login-administrativo/dashboard-veterinarios', component: DashboardVeterinarioComponent},
+
+  // CRUD Mascotas:
+  {path: 'login-administrativo/dashboard-veterinarios/crear-mascota', component: CrearMascotaComponent},
 ];
 
 @NgModule({
--- DrPawsRemastered/DrPawsRemastered/src/app/app.module.ts
+++ DrPawsRemastered/DrPawsRemastered/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { LandingPageComponent } from './landing-page/landing-page.component';
 import { LoginAdministrativoComponent } from './login-administrativo/login-administrativo.component';
 import { DashboardVeterinarioComponent } from './dashboard-veterinario/dashboard-veterinario.component';
+import { CrearMascotaComponent } from './crud-mascotas/crear-mascota/crear-mascota.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LandingPageComponent,
     LoginAdministrativoComponent,
-    DashboardVeterinarioComponent
+    DashboardVeterinarioComponent,
+    CrearMascotaComponent
   ],
   imports: [
     BrowserModule,
