4728df2096008817c68dce7edcccc9bd9755f513
Alberto Vigna <betoveluis@gmail.com>
2023-10-12 22:54:58 -0500

    Mejoras en el login del veterinario, modal funcionando.

--- DrPawsRemastered/DrPawsRemastered/src/app/logins/login-administrativo/login-administrativo.component.ts
+++ DrPawsRemastered/DrPawsRemastered/src/app/logins/login-administrativo/login-administrativo.component.ts
@@ -15,12 +15,16 @@ export class LoginAdministrativoComponent
     contrasenna: ''
   };
 
+  mostrarError: boolean = false;
+
   constructor(
     private autenticacionService: LoginVeterinarioService,
     private router: Router
   ) { }
 
   login(): void {
+    this.mostrarError = false; // Asegurándonos de que el error no se muestra antes de intentar autenticar
+
     this.autenticacionService.autenticarVeterinario(this.credenciales).subscribe(
       data => {
         console.log('Autenticación exitosa!', data);
@@ -28,8 +32,12 @@ export class LoginAdministrativoComponent
       },
       error => {
         console.log('Error en autenticación', error);
-        alert('Credenciales incorrectas');
-        // Manejar error (mostrar mensaje al usuario, etc.)
+        this.mostrarError = true; // Mostramos el error cuando falla la autenticación
+
+        // Si deseas que el mensaje de error se oculte automáticamente después de algunos segundos, puedes usar setTimeout.
+        setTimeout(() => {
+          this.mostrarError = false;
+        }, 6000); // Por ejemplo, aquí se oculta después de 6 segundos
       }
     );
   }
